ba96c8c5e81961798a68c37883be535829bd93d1
csrebeka97 <csrebeka97@gmail.com>
2022-04-06 01:32:59 +0200

    readingtable initial

--- frontend/src/pages/readingtable.js
+++ frontend/src/pages/readingtable.js
@@ -1,18 +1,59 @@
-import React from 'react';
+import './readingtable.css';
+
+import axios from 'axios';
+import {wrapper} from 'axios-cookiejar-support';
+import {CookieJar} from 'tough-cookie';
+import React, {useEffect, useState, useRef, Component} from 'react';
+import Cookies from 'js-cookie';
+import { Link} from "react-router-dom";
+import Select from 'react-select';
+import Table from 'react-bootstrap/Table';
+import {Bar} from 'react-chartjs-2';
+
+const jar = new CookieJar();
+const client = wrapper(axios.create({jar}));
+client.defaults.withCredentials = true
+window.testclient = client;
 
 const ReadingTable = () => {
+
+	const [userdata, setUserData] = useState("");
+
+	
+	useEffect(() => {
+        client.get(`http://localhost:8765/profiledata`)
+            .then(res => {
+                setUserData(res.data)
+            });
+    }, [])
+
+	if (userdata.username === ""){
+		return (
+			<div >
+				<div>
+				<br />
+				<br />
+				</div>
+				<div id="loginneeded">
+				<h1>You must login to see your profile</h1>
+				<button id="loginbutton">
+       			 <Link to="/login">
+              			Login
+
+            	 </Link> 
+       </button> 
+			</div>
+			</div>
+		)
+	}
+	else {
 return (
-	<div
-	style={{
-		display: 'flex',
-		justifyContent: 'Right',
-		alignItems: 'Right',
-		height: '100vh'
-	}}
-	>
-	<h1>Welcome to GeeksforGeeks</h1>
+	<div id="container">
+		<div id="search"></div>
+		<div id="readingtable"></div>
+		<div id="sugg"></div>
 	</div>
 );
-};
+}};
 
 export default ReadingTable;
